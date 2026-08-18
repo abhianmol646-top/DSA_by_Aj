82b66345a0257f3ae1e36665073344c0e78731ed
Anmol jain <abhianmol646@gmail.com>
2026-08-18 23:20:11 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   int reverse(int x) {
+        int r;
+        int rev=0;
+        while(x!=0)  
+        {
+          
+          r=x%10;
+         
+          if(rev>INT_MAX/10 || rev<INT_MIN/10) 
+          {
+            return 0;
+          }
+           rev=rev*10+r; 
+          x=x/10;   
+          
+          
+        }
+    return rev;
+}
+   
+};
+```
